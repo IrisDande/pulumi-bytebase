7cdb58792f0a2f153e76c83a2f9864e4dfe03fd6
IrisDande <cong.nguyenvan@hotmail.com>
2024-03-02 13:16:07 +0000

    update code

--- provider/cmd/pulumi-gen-bytebase/main.go
+++ provider/cmd/pulumi-gen-bytebase/main.go
@@ -1,80 +1,110 @@
+// Copyright 2016-2021, Pulumi Corporation.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package main
 
 import (
-	"bytes"
 	"encoding/json"
-	"flag"
 	"fmt"
+	"io/ioutil"
 	"os"
+	"path/filepath"
 
-	bytebase "github.com/IrisDande/pulumi-bytebase/provider/pkg/bytebase"
-	providerVersion "github.com/IrisDande/pulumi-bytebase/provider/pkg/version"
+	"github.com/pkg/errors"
+	dotnetgen "github.com/pulumi/pulumi/pkg/v3/codegen/dotnet"
+	gogen "github.com/pulumi/pulumi/pkg/v3/codegen/go"
+	nodejsgen "github.com/pulumi/pulumi/pkg/v3/codegen/nodejs"
+	pygen "github.com/pulumi/pulumi/pkg/v3/codegen/python"
+	"github.com/pulumi/pulumi/pkg/v3/codegen/schema"
 )
 
-// copied from encoding/json for use with JSONMarshal above
-func MarshalIndent(v any) ([]byte, error) {
-
-	// json.Marshal normally escapes HTML. This one doesn't
-	// https://stackoverflow.com/questions/28595664/how-to-stop-json-marshal-from-escaping-and
-	buffer := &bytes.Buffer{}
-	encoder := json.NewEncoder(buffer)
-	encoder.SetEscapeHTML(false)
-	err := encoder.Encode(v)
-	if err != nil {
-		return nil, err
+func main() {
+	if len(os.Args) < 4 {
+		fmt.Printf("Usage: %s <language> <out-dir> <schema-file>\n", os.Args[0])
+		os.Exit(1)
 	}
-	b := buffer.Bytes()
 
-	// serialize and pretty print
-	var buf bytes.Buffer
-	prefix, indent := "", "    "
-	err = json.Indent(&buf, b, prefix, indent)
+	language, outdir, schemaPath := os.Args[1], os.Args[2], os.Args[3]
+
+	err := emitSDK(language, outdir, schemaPath)
 	if err != nil {
-		return nil, err
+		fmt.Printf("Failed: %s", err.Error())
 	}
-	return buf.Bytes(), nil
 }
 
-func main() {
-	flag.Usage = func() {
-		const usageFormat = "Usage: %s <schema-file>"
-		fmt.Fprintf(flag.CommandLine.Output(), usageFormat, os.Args[0])
-		flag.PrintDefaults()
-	}
-
-	var version string
-	flag.StringVar(&version, "version", providerVersion.Version, "the provider version to record in the generated code")
-
-	flag.Parse()
-	args := flag.Args()
-	if len(args) < 1 {
-		flag.Usage()
-		return
-	}
-	s, err := bytebase.Schema(version)
+func emitSDK(language, outdir, schemaPath string) error {
+	pkg, err := readSchema(schemaPath)
 	if err != nil {
-		panic(err)
+		return err
 	}
 
-	// sort keys
-	var arg map[string]any
-	err = json.Unmarshal([]byte(s), &arg)
+	tool := "Pulumi SDK Generator"
+	extraFiles := map[string][]byte{}
+
+	var generator func() (map[string][]byte, error)
+	switch language {
+	case "dotnet":
+		generator = func() (map[string][]byte, error) { return dotnetgen.GeneratePackage(tool, pkg, extraFiles) }
+	case "go":
+		generator = func() (map[string][]byte, error) { return gogen.GeneratePackage(tool, pkg) }
+	case "nodejs":
+		generator = func() (map[string][]byte, error) { return nodejsgen.GeneratePackage(tool, pkg, extraFiles) }
+	case "python":
+		generator = func() (map[string][]byte, error) { return pygen.GeneratePackage(tool, pkg, extraFiles) }
+	default:
+		return errors.Errorf("Unrecognized language %q", language)
+	}
+
+	files, err := generator()
 	if err != nil {
-		panic(err)
+		return errors.Wrapf(err, "generating %s package", language)
 	}
 
-	// remove version key
-	delete(arg, "version")
+	for f, contents := range files {
+		if err := emitFile(outdir, f, contents); err != nil {
+			return errors.Wrapf(err, "emitting file %v", f)
+		}
+	}
 
-	// use custom marshal indent to skip html escaping
-	out, err := MarshalIndent(arg)
+	return nil
+}
+
+func readSchema(schemaPath string) (*schema.Package, error) {
+	schemaBytes, err := ioutil.ReadFile(schemaPath)
 	if err != nil {
-		panic(err)
+		return nil, errors.Wrap(err, "reading schema")
 	}
 
-	schemaPath := args[0]
-	err = os.WriteFile(schemaPath, out, 0600)
-	if err != nil {
-		panic(err)
+	var spec schema.PackageSpec
+	if err = json.Unmarshal(schemaBytes, &spec); err != nil {
+		return nil, errors.Wrap(err, "unmarshalling schema")
 	}
+
+	pkg, err := schema.ImportSpec(spec, nil)
+	if err != nil {
+		return nil, errors.Wrap(err, "importing schema")
+	}
+	return pkg, nil
+}
+
+func emitFile(rootDir, filename string, contents []byte) error {
+	outPath := filepath.Join(rootDir, filename)
+	if err := os.MkdirAll(filepath.Dir(outPath), 0755); err != nil {
+		return err
+	}
+	if err := ioutil.WriteFile(outPath, contents, 0600); err != nil {
+		return err
+	}
+	return nil
 }
